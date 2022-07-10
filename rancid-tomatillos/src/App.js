eb05534684867ee730d140b41e6bef47227c07b1
Amanda Sands <amandajean555@gmail.com>
2022-07-10 17:58:11 -0600

    Add initial API fetch call and some error handling

--- rancid-tomatillos/src/App.js
+++ rancid-tomatillos/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React, {Component} from "react";
-import movieData from "./sample-data/movie-data.js"
 import Navbar from "./Components/Navbar/Navbar"
 import CardContainer from "./Components/CardContainer/CardContainer"
 import MovieView from "./Components/MovieView/MovieView"
@@ -9,16 +8,32 @@ class App extends Component {
   constructor() {
     super();
     this.state= {
-      allMovies: movieData.movies,
+      allMovies: [],
       isClicked: false,
-      selectedMovie: {}
+      selectedMovie: {},
+      error: "",
+      loading: false,
       }
     };
   
+  componentDidMount() {
+    this.setState({
+      loading: true,
+    })
+    fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
+    .then(response => response.json())
+    .then(data => {
+      console.log("Data", data)
+      this.setState({allMovies: data.movies, loading: false})
+    .catch(() =>
+      this.setState({error: "There was an error loading your films. Please try again!"}) 
+      )
+    })
+  }
 
-   handleClick = (id) => {
-     let selectedMovie = this.state.allMovies.find(movie => movie.id === id)
-     this.setState({selectedMovie: selectedMovie, isClicked: true})
+  handleClick = (id) => {
+    let selectedMovie = this.state.allMovies.find(movie => movie.id === id)
+    this.setState({selectedMovie: selectedMovie, isClicked: true})
       console.log("SELECTED MOVIE", selectedMovie)
       console.log("ID", id);
     }
@@ -31,11 +46,13 @@ class App extends Component {
     
   render() {
   return (
-       <div>
-          <Navbar returnToMain={this.returnToMain}/>
+      <div>
+        <Navbar returnToMain={this.returnToMain}/>
+        {this.state.error && <h3>{this.state.error}</h3>}
+        {!this.state.allMovies.length && !this.state.error && <h3>Loading...</h3>}
         {this.state.isClicked ? <MovieView selectedMovie={this.state.selectedMovie} /> :
         <CardContainer movies={this.state.allMovies} handleClick={this.handleClick} />}
-        </div>
+      </div>
     )
   }
 
