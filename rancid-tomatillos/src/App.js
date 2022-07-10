6bdbb0dcb2770e8f01e21d8ce947f2168e1d6f5a
Jordan Sullivan <95270427+jordan-sullivan@users.noreply.github.com>
2022-07-10 14:27:16 -0600

    Render movie view data to MovieView page

--- rancid-tomatillos/src/App.js
+++ rancid-tomatillos/src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import React, {Component} from "react";
 import movieData from "./sample-data/movie-data.js"
 import Navbar from "./Components/Navbar/Navbar"
-import CardContainer from "./Components/CardContainer/CardContainer"
-//import MovieView from "./Components/MovieView/MovieView"
+// import CardContainer from "./Components/CardContainer/CardContainer"
+import MovieView from "./Components/MovieView/MovieView"
 
 class App extends Component {
   constructor() {
@@ -11,25 +11,25 @@ class App extends Component {
     this.state= {
       allMovies: movieData.movies,
       isClicked: false,
-      selectedMovie:{},
+
+      selectedMovie: { id: 1, title: "Fake Movie Title", 
+      poster_path: "https://image.tmdb.org/t/p/original//7G2VvG1lU8q758uOqU6z2Ds0qpA.jpg", 
+      backdrop_path: "https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg", 
+      release_date: "2019-12-04", overview: "Some overview that is full of buzzwords to attempt to entice you to watch this movie! Explosions! Drama! True love! Robots! A cute dog!", 
+      average_rating: 6, genres: ["Drama"], budget: 63000000, 
+      revenue: 100853753, runtime: 139, tagline: "It's a movie!"}
+      }
     };
-  }
+  
 
-  handleClick = (id) => {
-    let selectedMovie = this.state.allMovies.find(movie => movie.id === id)
-    this.setState({selectedMovie: selectedMovie})
-    console.log("ID", id);
-}
+//    handleClick = (id) => {
 
-    //hides main page
-    //brings us to MV page
-    
-    // if id of what is clicked on matches id on data set, 
-    //then, display that id's data in MV
-    //based on the id, ids need to match the data 
-    //nav bar still available
+//      let selectedMovie = this.state.allMovies.find(movie => movie.id === id)
+//      this.setState({selectedMovie: selectedMovie, isClicked: true})
+//     console.log("SELMOV", selectedMovie)
+//     console.log("ID", id);
+//  }
 
-    //this.setState({isClicked})
 
 
     
@@ -37,13 +37,13 @@ class App extends Component {
     return (
       <div>
         <Navbar />
-        <CardContainer movies={this.state.allMovies} handleClick={this.handleClick}/>
-        {/* <MovieView /> */}
+        {/* <CardContainer movies={this.state.allMovies} handleClick={this.handleClick}/> */}
+        <MovieView selectedMovie={this.state.selectedMovie}/>
       </div>
     )
   }
-}
 
+}
 
 
 export default App;
--- rancid-tomatillos/src/Components/MovieView/MovieView.js
+++ rancid-tomatillos/src/Components/MovieView/MovieView.js
@@ -1,24 +1,23 @@
 import React from "react";
 import "./MovieView.css"
-
-const MovieView = () => {
-
-     
+//selectedMovie
+const MovieView = ({selectedMovie}) => {
 
     
+
     return(
         <div className="movieViewMain">
             <div className="posterTitleContainer">
-            <h2>Fancy Movie Title here</h2>
-            <img alt="movie" src="https://image.tmdb.org/t/p/original//oazPqs1z78LcIOFslbKtJLGlueo.jpg" />
+                <h2>{selectedMovie.title}</h2>
+                <img className="backdrop" alt="movie" src={selectedMovie.backdrop_path} />
             </div>
         <div className="statsContainer">
-            <p className="releaseDate">releaseDate</p>
-            <p className="tagline">tagline</p>
-            <p className="rating">rating</p>
-            <p className="budget">budget</p>
-            <p className="revenue">revenue</p>
-            <p className="runtime">runtime</p>
+                <p className="releaseDate">{selectedMovie.release_date}</p>
+            <p className="tagline">{selectedMovie.tagline}</p>
+            <p className="rating">{selectedMovie.average_rating} ⭐️</p>
+            <p className="budget">$ {selectedMovie.budget}</p>
+                <p className="revenue">$ {selectedMovie.revenue}</p>
+                <p className="runtime">{selectedMovie.runtime} mins.</p>
         </div>
         <footer></footer>
     </div>
