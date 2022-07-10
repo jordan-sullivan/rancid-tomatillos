b644531c3cac63d3926b462d65be40be6457f5a2
Jordan Sullivan <95270427+jordan-sullivan@users.noreply.github.com>
2022-07-10 11:52:37 -0600

    Change Card class component to functional component

--- rancid-tomatillos/src/Components/Card/Card.js
+++ rancid-tomatillos/src/Components/Card/Card.js
@@ -1,23 +1,17 @@
 import './Card.css';
-import React, {Component} from "react";
+import React from "react";
 
-class Card extends Component {
-    constructor(props) {
-    super(props); 
-    this.state = {};   
-    }
-    
-    render() {
+const Card = ({key, poster, rating, title}) => {
+
         return (
-            <div className="card"> here is card 
-                <div className="poster"><img class="posterImage" src={this.props.poster}/></div>
+            <div className="card">
+                <div className="poster"><img class="posterImage" src={poster}/></div>
                    <div className="cardDescription">
-                        <p>Title: {this.props.title}</p>
-                        <p>Rating: {this.props.rating.toFixed(1)}</p>
+                        <p>Title: {title}</p>
+                        <p>Rating: {rating.toFixed(1)}</p>
                     </div>
             </div>
         )
-    }
 }
 
 export default Card;
