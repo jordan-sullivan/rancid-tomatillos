543c177ea78292f22be7eb3983333f86aaf2b151
Jordan Sullivan <95270427+jordan-sullivan@users.noreply.github.com>
2022-07-13 16:20:17 -0600

    set up testing environment for cypress

---
+++ rancid-tomatillos/cypress/e2e/app.cy.js
@@ -0,0 +1,9 @@
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+    
+  })  it('Should confirm that true is equal to true', () => {
+    expect(true).to.equal(true)
+  });
+
+})
