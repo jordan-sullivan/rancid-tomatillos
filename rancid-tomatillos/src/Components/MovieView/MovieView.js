db7da09076a76ac6106337ce85cec67b340c7503
Amanda Sands <amandajean555@gmail.com>
2022-07-15 20:29:26 -0600

    Format display for movieView component

--- rancid-tomatillos/src/Components/MovieView/MovieView.js
+++ rancid-tomatillos/src/Components/MovieView/MovieView.js
@@ -11,6 +11,25 @@ class MovieView extends Component{
         };
     }
 
+    formatDate() {
+        let testRelease = this.state.selectedMovieInfo.release_date
+        return new Date(testRelease).toLocaleString().split(' ')[0]
+    }
+
+    formatGenres() {
+        return (new Intl.ListFormat("en-GB", {style: "long", type: "conjunction"}).format(this.state.selectedMovieInfo.genres))
+    }
+
+    translateToCurrency(number) {
+        return `$${new Intl.NumberFormat().format(number)}`;
+    }
+
+    formatRuntime() {
+        let testRuntime = this.state.selectedMovieInfo.runtime
+        let hoursAndMins = (testRuntime/60).toFixed(2).split('.');
+        return `${hoursAndMins[0]} hour and ${(hoursAndMins[1] * .60).toFixed(0)} minutes`
+    }
+
     componentDidMount() {
         fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.id}`)
         .then(response => response.json())
@@ -25,23 +44,24 @@ class MovieView extends Component{
     }
     
     render() {
+        let film = this.state.selectedMovieInfo;
         return(
             <div className="movieViewError">
             <div style={{display: this.state.error ? "block" : "none"}}> {this.state.error} </div>
-            <div className="movieViewMain" src={this.state.selectedMovieInfo.backdrop_path} 
-                                        style={{ backgroundImage: `url(${this.state.selectedMovieInfo.backdrop_path})` }}>
+            <div className="movieViewMain" src={film.backdrop_path} 
+                                        style={{ backgroundImage: `url(${film.backdrop_path})` }}>
                 <div className="posterTitleContainer">
                     <div className="floater">
                         <div className="statsContainer">
-                            <h2 className="title">{this.state.selectedMovieInfo.title}</h2>
-                            <p className="tagline">Tagline: {this.state.selectedMovieInfo.tagline}</p>
-                            <p className="releaseDate">Released: {this.state.selectedMovieInfo.release_date}</p>
-                            <p className="budget">Budget: {this.state.selectedMovieInfo.budget}</p>
-                            <p className="revenue">Revenue: {this.state.selectedMovieInfo.revenue}</p>
-                            <p className="runtime">Runtime: {this.state.selectedMovieInfo.runtime} mins.</p>
-                            <p className="rating">{this.state.selectedMovieInfo.average_rating} ⭐️</p>
+                            <h2 className="title">{film.title}</h2>
+                            <p className="tagline">Tagline: {film.tagline}</p>
+                            <p className="releaseDate">Released: {this.formatDate()}</p>
+                            <p className="budget">Budget: {this.translateToCurrency(film.budget)}</p>
+                            <p className="revenue">Revenue: {this.translateToCurrency(film.revenue)}</p>
+                            <p className="runtime">Runtime: {this.formatRuntime()}</p>
+                            <p className="rating">{film.average_rating} ⭐️</p>
                             <div className="genresHolder">
-                                <p className="genres">{this.state.selectedMovieInfo.genres}</p>
+                                <p className="genres">{this.formatGenres()}</p>
                             </div>
                         </div>
                     </div>
